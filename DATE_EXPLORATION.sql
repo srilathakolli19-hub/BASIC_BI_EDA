63d12b6ea3769eab6f4d0f9afaf9a99b09cccbdb
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-10 19:16:56 +0530

    Update DATE_EXPLORATION.sql

--- DATE_EXPLORATION.sql
+++ DATE_EXPLORATION.sql
@@ -1,1 +1,17 @@
+--date exploration 
 
+--find the first and last order
+--find how many years the sales are happening
+select 
+MIN(order_date) as first_order_date,
+MAX(order_date) as last_order_date,
+DATEDIFF(year,min(order_date),MAX(order_date)) as order_range_years
+from gold.fact_sales
+
+--find the oldest and youngest customer
+select 
+MIN(birthdate) as oldest_birthdate,
+DATEDIFF(year,min(birthdate),getdate()) as oldest_age,
+MAX(birthdate) as youngest_birthdate,
+DATEDIFF(year,max(birthdate),getdate()) as youngest_age
+from gold.dim_customers
